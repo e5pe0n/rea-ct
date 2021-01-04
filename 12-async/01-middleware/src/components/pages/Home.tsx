e29a6f74047a56704e7577dce5d0fea0c99adb47
1izard <1izard.synchr0fi5h@gmail.com>
2021-01-04 11:42:17 +0000

    add

---
+++ 12-async/01-middleware/src/components/pages/Home.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import capitalize from 'lodash/capitalize';
+import { List } from 'semantic-ui-react';
+
+import './Home.css';
+
+const Home: FC<{ orgCodes: string[] }> = ({ orgCodes = [] }) => (
+  <>
+    <header className="app-header">
+      <h1>Companies</h1>
+    </header>
+    <List celled relaxed>
+      {orgCodes.map((orgCode) => (
+        <List.Item className="list-item" keys={orgCode}>
+          <List.Icon name="users" size="large" verticalAlign="middle" />
+          <List.Content>
+            <Link to={`/${orgCode}/members`}>
+              Members of {capitalize(orgCode)}
+            </Link>
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  </>
+);
+
+export default Home;
